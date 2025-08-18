45a0b66c6e52613a8a9ab26cc5b59e0f4ae176e7
Damian-MG <malenogon_f@hotmail.com>
2025-08-18 20:52:08 +0200

    agent langgraph mcp server

--- langgraph_mcp_server/client.py
+++ langgraph_mcp_server/client.py
@@ -14,3 +14,82 @@ from langgraph.graph import StateGraph
 from my_mcp.config import mcp_config
 from langgraph_mcp_server.graph import build_agent_graph, AgentState
 
+
+async def stream_graph_response(
+        input: AgentState, graph: StateGraph, config: dict = {}
+        ) -> AsyncGenerator[str, None]:
+    """
+    Stream the response from the graph while parsing out tool calls.
+
+    Args:
+        input: The input for the graph
+        graph: The graph to run
+        config: The config to pass to the graph. Required for memory
+
+    Yields:
+        A processed string from the graph's chunked response
+    """
+    async for message_chunk, metadata in graph.astream(input=input,
+        stream_mode="messages", config=config):
+        if isinstance(message_chunk, AIMessageChunk):
+            if message_chunk.response_metadata:
+                finish_reason = message_chunk.response_metadata.get(
+                    "finish_reason", "")
+                if finish_reason == "tool_calls":
+                    yield "\n\n"
+            
+            if message_chunk.tool_call_chunks:
+                tool_chunk = message_chunk.tool_call_chunks[0]
+                tool_name = tool_chunk.get("name", "")
+                args = tool_chunk.get("args", "")
+
+                if tool_name:
+                    tool_call_str = f"\n\n <TOOL CALL: {tool_name} > \n\n"
+                if args:
+                    tool_call_str = args
+
+                yield tool_call_str
+
+            else:
+                yield message_chunk.content
+
+            continue
+
+
+async def main():
+    """
+    Initialize the MCP Client and run the agent conversation loop.
+
+    The MultiServerMCPClient allows the connection to multiple MCP servers
+    using a single client and config.
+    """
+    async with MultiServerMCPClient(connections=mcp_config) as client:
+        tools = client.get_tools()
+        graph = build_agent_graph(tools=tools)
+
+        graph_config = {
+            "configurable": {
+                "thread_id" : "1"
+            }
+        }
+
+        while True:
+            user_input = input("\n\n USER: ")
+            if user_input in ["quit", "exit"]:
+                break
+
+            print("\n ------ USER ------ \n\n", user_input)
+            print("\n ------ ASSISTANT ------ \n\n")
+
+            async for response in stream_graph_response(
+                input=AgentState(messages=[HumanMessage(content=user_input)]),
+                graph=graph,
+                config=graph_config
+            ):
+                print(response, end="", flush=True)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
